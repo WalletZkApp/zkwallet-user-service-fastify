79afccba9a91a36e1007b5c0fc59b9a0a346e62e
iam-dev <>
2023-07-11 21:16:31 +0200

    Add _printBalances

--- src/onchain/onchain.service.ts
+++ src/onchain/onchain.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AccountUpdate, Mina, PublicKey, PrivateKey } from 'snarkyjs';
+import {
+  AccountUpdate,
+  Mina,
+  PublicKey,
+  PrivateKey,
+  fetchAccount,
+} from 'snarkyjs';
 import { AllConfigType } from 'src/config/config.type';
 
 @Injectable()
@@ -15,24 +21,42 @@ export class OnchainService {
         infer: true,
       },
     ) as string;
-    this.privateKey = PrivateKey.fromBase58(privateKeyFromConf);
-    this.publicKey = this.privateKey.toPublicKey();
-
     const berkeley = Mina.Network(
       'https://proxy.berkeley.minaexplorer.com/graphql',
     );
     Mina.setActiveInstance(berkeley);
+
+    this.privateKey = PrivateKey.fromBase58(privateKeyFromConf);
+    this.publicKey = this.privateKey.toPublicKey();
   }
 
   async sendMina(receiver: string, amount: number): Promise<boolean> {
+    console.log(`Sending ${amount} Mina to ${receiver}`);
     const receiverPublicKey: PublicKey = PublicKey.fromBase58(receiver);
+
+    const response = await fetchAccount({ publicKey: this.publicKey });
+    if (response.error) throw Error(response.error.statusText);
+    const { nonce, balance } = response.account;
+    console.log(
+      `Using fee payer account with nonce ${nonce}, balance ${balance}`,
+    );
 
     const tx = await Mina.transaction(this.publicKey, () => {
       const accountUpdate = AccountUpdate.fundNewAccount(this.publicKey);
-      accountUpdate.send({ to: receiverPublicKey, amount: amount });
+      accountUpdate.send({ to: receiverPublicKey, amount: 1e9 * amount });
     });
     await tx.sign([this.privateKey]).send();
+    this._printBalances(receiverPublicKey);
 
     return true;
+  }
+
+  private _printBalances(account: PublicKey) {
+    console.log(
+      `account1 balance: ${Mina.getBalance(this.publicKey).div(1e9)} MINA`,
+    );
+    console.log(
+      `account2 balance: ${Mina.getBalance(account).div(1e9)} MINA\n`,
+    );
   }
 }
