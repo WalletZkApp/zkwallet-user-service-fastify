aca457c280f7ad76f0322d29047faedda7952243
iam-dev <>
2023-07-11 16:16:08 +0200

    Add stripes

--- src/topup/topup.controller.ts
+++ src/topup/topup.controller.ts
@@ -1,13 +1,34 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { PaymentRequestBody } from './types/PaymentRequestBody';
 import { TopupService } from './topup.service';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/roles/roles.decorator';
+import { RoleEnum } from 'src/roles/roles.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/roles/roles.guard';
 
-@Controller('topup')
+@ApiBearerAuth()
+@Roles(RoleEnum.admin)
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@ApiTags('Topups')
+@Controller({
+  path: 'topup',
+  version: '1',
+})
 export class TopupController {
   constructor(private topupService: TopupService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   createPayments(
     @Res() response: Response,
     @Body() paymentRequestBody: PaymentRequestBody,
